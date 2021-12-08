1892ca6596f8edf75b7177a9ade51aa4d55ef648
SoluWeb <csantacruz@valledelcauca.gov.co>
2021-12-08 12:26:22 -0500

    Definición base de rutas en el frontend

--- proyectosoluweb-frontend/src/App.js
+++ proyectosoluweb-frontend/src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import NuevoUsuario from './components/NuevoUsuario';
+import Usuarios from './components/Usuarios';
+import Registro from './components/Registro';
+import Login from './components/Login';
+import Proyectos from './components/Proyectos';
+import Inscripciones from './components/Inscripciones';
+import Avances from './components/Avances';
+
 function App() {
   return (
-    <center><h1>Equipo SoluWeb</h1></center>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Registro" element={<Registro />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/NuevoUsuario" element={<NuevoUsuario />} />
+        <Route path="/Usuarios" element={<Usuarios />} />
+        <Route path="/Proyectos" element={<Proyectos />} />
+        <Route path="/Inscripciones" element={<Inscripciones />} />
+        <Route path="/Avances" element={<Avances />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

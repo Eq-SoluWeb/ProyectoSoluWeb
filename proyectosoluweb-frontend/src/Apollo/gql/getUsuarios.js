65c91c1b2afdece1640e4258eb10f9689a610bfe
mariaisabeles <mariaisabel.es@hotmail.com>
2021-12-10 15:06:21 -0500

    Creación de query usuarios

---
+++ proyectosoluweb-frontend/src/Apollo/gql/getUsuarios.js
@@ -0,0 +1,14 @@
+import {gql} from '@apollo/client';
+
+const GET_USUARIOS=gql`query{
+    Usuarios
+    {
+      email
+      identificacion
+      nombreCompleto
+      estado
+      password
+      rol
+    }
+  }`;
+  export default GET_USUARIOS
